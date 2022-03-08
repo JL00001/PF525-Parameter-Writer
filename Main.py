2100fdc08f742cc19cb95981f17c3239e52eadd2
JL00001 <44040750+JL00001@users.noreply.github.com>
2022-03-08 13:31:25 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,28 +1,10 @@
 import Objects
-import pycomm3
-import ipaddress
-
 
 
 def main():
 
-    network = Objects.NetworkObject()
-    #network.network_parameter_dump("Test")
-    #network.network_drive_health()
-    #print(network.list_of_drive_objects["172.16.11.11"].detailed_read_all_pf525_data())
-
-
-    write_params = [
-        (573, 3),
-        (41, 100),
-        (42, 100)]
-    network.list_of_drive_objects["172.16.21.14"].scattered_write(write_params)
-
-
-
-
-
-
+    network = Objects.NetworkObject()    
+    network.network_drive_health()
 
 
 
@@ -30,23 +12,37 @@ if __name__ == "__main__":
     main()
 
 
+"""
+network.Dev_Add_Drive("192.168.1.1")
+network.list_of_drive_objects["192.168.1.1"] 
+network.load_parameters_from_excel("Full Drive Parameter Template.xlsx")
+network.network_autoTune_drive()
+network.network_drive_health()
+network.network_masked_autoTune_drive(Network, mask)
+network.network_parameter_dump()
+network.network_parameter_search(41, 100)
+network.network_read_parameter(41)
+network.network_write_parameter(41,100)
+network.single_read_parameter("192.168.1.1",41)
+network.single_write_parameter("192.168.1.1",41,100)
+"""
 
-#network.network_AutoTuned()
-#network.network_drive_health()
-#network.single_drive_amazon("11.200.3.21")
-#network.single_drive_auto("11.200.3.21")
-#network.network_drive_write(157,3)
-#network.single_drive_read("172.16.18.12",42)
-#network.network_parameter_search(41, 1)
-#network.network_parameter_search(42, 1)
-#network.single_write_parameter("172.16.18.12",40,1)
-#network.single_drive_auto("172.16.18.12")
-#network.single_drive_manual("172.16.18.12")
-#network.single_drive_autotune("172.16.18.12")
 
-#network.all_network_drive_autoTune()
-#network.network_masked_drive_autoTune("172.16.41.16","255.255.255.252")
-#network.network_parameter_dump("Test")
-#print(network.list_of_drive_objects["172.16.41.17"].read_all_pf525_data())
-#network.list_of_drive_objects["172.16.41.17"].read_all_pf525_data()
-#network.load_Parameters_from_excel("Parameter.xlsx")
+
+
+"""
+network.list_of_drive_objects["192.168.1.1"].detailed_read_all_pf525_data()
+network.list_of_drive_objects["192.168.1.1"].drive_autotune()
+network.list_of_drive_objects["192.168.1.1"].read_all_pf525_data()
+network.list_of_drive_objects["192.168.1.1"].read_pf525_logic_command()
+network.list_of_drive_objects["192.168.1.1"].read_pf525_parameter(41)
+network.list_of_drive_objects["192.168.1.1"].scattered_read([496,498,500])
+
+write_params = [
+            (496, 0),
+            (498, 0),
+            (500, 0)]
+     
+network.list_of_drive_objects["192.168.1.1"].scattered_write(write_params)
+network.list_of_drive_objects["192.168.1.1"].write_pf525_parameter(41,100)
+"""
